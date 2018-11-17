1a0f2400618a3627ba6ebc719be3cfd39f54ce70
Manoj <manoj.delhi24@gmail.com>
2018-11-17 17:27:12 +0700

    curried version HOF nTimes

--- src/lecture/part3fp/HOFsCurries.scala
+++ src/lecture/part3fp/HOFsCurries.scala
@@ -14,4 +14,15 @@ object HOFsCurries extends App {
 
   // Better way to implement this nTimes function
 
+  // fn nTimes take a fn and take number and returns another fn which is application
+  // of this fn ntimes which we can then use it for any value we want.
+  val incrOne = (x: Int) => x + 1
+  
+  def nTimesNew(fn: Int => Int, n: Int): Int => Int =
+    if (n <= 0) (x: Int) => x
+    else (x: Int) => nTimesNew(fn, n - 1)(fn(x))
+
+  val plusSix = nTimesNew(incrOne, 6)
+  println(plusSix(26))
+
 }
