2ffa288d9869078d6feff960f95c6d4c6a08494b
Manoj <manoj.delhi24@gmail.com>
2018-12-05 18:06:51 +0700

    Maps and its commonly used methods

--- src/lecture/part3fp/TuplesAndMaps.scala
+++ src/lecture/part3fp/TuplesAndMaps.scala
@@ -9,4 +9,14 @@ object TuplesAndMaps extends App {
   println(aTuple._2)
   println(aTuple.copy(_2 = "Tada!"))
   println(anotherTuple.swap)
+
+  // Maps - key -> value
+  val aMap: Map[String, Int] = Map()
+  val phoneBook = Map(("tom", 241), "jerry" -> 1124) // "jerry" -> 1124 is syntactic sugar
+  println(phoneBook)
+  println(phoneBook.map(pair => pair._1.toUpperCase -> pair._2))
+  println(phoneBook.filter(pair => pair._2 % 2 == 0))
+  println(phoneBook.filterKeys(key => key.contains('m')))
+  println(phoneBook.mapValues(v => v + "-**"))
+  println(phoneBook.keys.groupBy(name => name.charAt(0)))
 }
