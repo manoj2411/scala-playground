817ebb1d2a1f69989c89e4cf6dda81a0e3389d14
Manoj <manoj.delhi24@gmail.com>
2019-04-08 13:51:06 +0700

    Creating fix sized threadpool using Executors.

--- src/lecture/advscalap3concurrency/Intro.scala
+++ src/lecture/advscalap3concurrency/Intro.scala
@@ -1,18 +1,40 @@
 package lecture.advscalap3concurrency
 
+import java.util.concurrent.Executors
+
 object Intro extends App {
 
   val runnable = new Runnable {
     override def run(): Unit = println("Inside runnable!")
   }
   val aThread = new Thread(runnable)
-  aThread.start() // Gives signal to JVM to START a JVM thread.
-  runnable.run() // BAD: doesn't do anything in parallel. call it via thread.start
-  aThread.join() // BLOCKS until aThread finishes running.
+  // aThread.start() // Gives signal to JVM to START a JVM thread.
+  // runnable.run() // BAD: doesn't do anything in parallel. call it via thread.start
+  // aThread.join() // BLOCKS until aThread finishes running.
 
   val helloThreads = new Thread(() => (1 to 5).foreach(_ => println("hello")))
   val byeThreads = new Thread(() => (1 to 5).foreach(_ => println("Bye")))
-  helloThreads.start
-  byeThreads.start
+  // helloThreads.start
+  // byeThreads.start
 
+  // Note: with JVM - Its very expensive to start and kill a thread. The solution is to reuse them.
+  // ## Executors - Its a part of java.util.concurrent package.
+  val pool = Executors.newFixedThreadPool(10)
+
+  pool.execute(() => {
+    Thread.sleep(1000)
+    println("after 1 sec")
+  })
+  pool.execute(() => {
+    Thread.sleep(1000)
+    println("almost after 1 sec")
+  })
+  pool.execute(() => {
+    Thread.sleep(2000)
+    println("I slept 2 secs :)")
+  })
+
+  pool.shutdown() // graceful shutdown
+  // pool.shutdownNow() // force shutdown and raises "sleep interrupted" exception
+  println(pool.isShutdown)
 }
