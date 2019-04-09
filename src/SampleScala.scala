9b0ac28de4083f80ed3c32657cf0e7547d20169e
Manoj <manoj.delhi24@gmail.com>
2019-04-09 22:48:47 +0700

    Solving concurrency problem with synchronized method on ref.

--- src/SampleScala.scala
+++ src/SampleScala.scala
@@ -1,31 +1,12 @@
 
 object SampleScala extends App {
-  import abc._
   //  1.
   //  val concatString = new Function2[String, String, String] {
   def concatString: (String, String) => String = new Function2[String, String, String] {
     override def apply(str1: String, str2: String) = str1 + str2
   }
-  println(concatString("Manoj", " Sehrawat"))
-
-  val c: A = new C
-  val d: A = new D
-  val a: A = new A
-
-  println("Is instance(a): " + a.isInstanceOf[B])
-  println("Is instance(c): " + c.isInstanceOf[B])
-  println("Is instance(d): " + d.getClass.getName )
-  println("Is instance(b): " + (new B).isInstanceOf[B])
-}
-
-package abc {
-  class A
-
-  class B extends A
-
-  class C extends B
-
-  class D extends A
+  //  println(concatString("Manoj", " Sehrawat"))
 
 }
+
 
--- src/lecture/advscalap3concurrency/Intro.scala
+++ src/lecture/advscalap3concurrency/Intro.scala
@@ -21,20 +21,61 @@ object Intro extends App {
   // ## Executors - Its a part of java.util.concurrent package.
   val pool = Executors.newFixedThreadPool(10)
 
-  pool.execute(() => {
-    Thread.sleep(1000)
-    println("after 1 sec")
-  })
-  pool.execute(() => {
-    Thread.sleep(1000)
-    println("almost after 1 sec")
-  })
-  pool.execute(() => {
-    Thread.sleep(2000)
-    println("I slept 2 secs :)")
-  })
+  // pool.execute(() => {
+  //   Thread.sleep(1000)
+  //   println("after 1 sec")
+  // })
+  // pool.execute(() => {
+  //   Thread.sleep(1000)
+  //   println("almost after 1 sec")
+  // })
+  // pool.execute(() => {
+  //   Thread.sleep(2000)
+  //   println("I slept 2 secs :)")
+  // })
 
-  pool.shutdown() // graceful shutdown
+  // pool.shutdown() // graceful shutdown
   // pool.shutdownNow() // force shutdown and raises "sleep interrupted" exception
-  println(pool.isShutdown)
+  // println(pool.isShutdown)
+
+  class BankAccount(var amount: Int) {
+    override def toString: String = s"$amount"
+  }
+
+  def buy(account: BankAccount, thing: String, price: Int) = {
+    account.amount -= price
+    //println(s"I've bought $thing")
+    //println(s"My acount is now $account")
+  }
+
+  for (i <- (1 to 2000)) {
+    val account = new BankAccount(5000)
+    val thread1 = new Thread(() => buy(account, "shoes", 2000))
+    val thread2 = new Thread(() => buy(account, "tShirt", 1000))
+    thread1.start
+    thread2.start
+
+    Thread.sleep(10)
+    if (account.amount != 2000 ) println(s"Dispute: ${account}")
+  }
+  // use synchronised() to solve concurrency problem (option1)
+  def safeBuy(account: BankAccount, thing: String, price: Int) = {
+    account.synchronized {
+      // with synchronized method, no 2 threads can evaluate this expression at the same time.
+      account.amount -= price
+    }
+  }
+  println("Safe buy:")
+  for (i <- (1 to 2000)) {
+    val account = new BankAccount(5000)
+    val thread1 = new Thread(() => safeBuy(account, "shoes", 2000))
+    val thread2 = new Thread(() => safeBuy(account, "tShirt", 1000))
+    thread1.start
+    thread2.start
+
+    Thread.sleep(10)
+    if (account.amount != 2000 ) println(s"Dispute: ${account}")
+  }
+
+
 }
