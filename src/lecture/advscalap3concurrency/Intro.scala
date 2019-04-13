81fe0c6e80108b9cca712e0883b5bab11d6cb0df
Manoj <manoj.delhi24@gmail.com>
2019-04-13 11:11:07 +0700

    Creating inception thread (thread creating other thread).

--- src/lecture/advscalap3concurrency/Intro.scala
+++ src/lecture/advscalap3concurrency/Intro.scala
@@ -48,16 +48,16 @@ object Intro extends App {
     //println(s"My acount is now $account")
   }
 
-  for (i <- (1 to 2000)) {
-    val account = new BankAccount(5000)
-    val thread1 = new Thread(() => buy(account, "shoes", 2000))
-    val thread2 = new Thread(() => buy(account, "tShirt", 1000))
-    thread1.start
-    thread2.start
-
-    Thread.sleep(10)
-    if (account.amount != 2000 ) println(s"Dispute: ${account}")
-  }
+//  for (i <- (1 to 2000)) {
+//    val account = new BankAccount(5000)
+//    val thread1 = new Thread(() => buy(account, "shoes", 2000))
+//    val thread2 = new Thread(() => buy(account, "tShirt", 1000))
+//    thread1.start
+//    thread2.start
+//
+//    Thread.sleep(10)
+//    if (account.amount != 2000 ) println(s"Dispute: ${account}")
+//  }
   // Solution #1 to race condition:
   //  use synchronised() to solve concurrency problem (option1)
   def safeBuy(account: BankAccount, thing: String, price: Int) = {
@@ -66,20 +66,37 @@ object Intro extends App {
       account.amount -= price
     }
   }
-  println("Safe buy:")
-  for (i <- (1 to 2000)) {
-    val account = new BankAccount(5000)
-    val thread1 = new Thread(() => safeBuy(account, "shoes", 2000))
-    val thread2 = new Thread(() => safeBuy(account, "tShirt", 1000))
-    thread1.start
-    thread2.start
-
-    Thread.sleep(10)
-    if (account.amount != 2000 ) println(s"Dispute: ${account}")
-  }
+//  println("Safe buy:")
+//  for (i <- (1 to 2000)) {
+//    val account = new BankAccount(5000)
+//    val thread1 = new Thread(() => safeBuy(account, "shoes", 2000))
+//    val thread2 = new Thread(() => safeBuy(account, "tShirt", 1000))
+//    thread1.start
+//    thread2.start
+//
+//    Thread.sleep(10)
+//    if (account.amount != 2000 ) println(s"Dispute: ${account}")
+//  }
 
   // Solution #2 to race condition:
   //  add annotation @volatile on a val or var, all reads/writes to val/var will be synchronised.
   class BankAccountSafe(@volatile var amount: Int) // Its less used since synchronised block give more control.
 
+  /* Exercise: construct 50 inception threads
+      Thread1 -> Thread2 -> ...
+      println("hello form thread #2")
+    in reverse order
+  */
+
+  def inceptionThreads(maxThreads: Int, currentCount: Int = 1): Thread = new Thread(() => {
+    if (maxThreads > currentCount) {
+      val currThread = inceptionThreads(maxThreads, currentCount + 1)
+      currThread.start
+      currThread.join // This is important, this holds the control until processing finishes.
+    }
+    println(s"Hello from thread #${currentCount}")
+  })
+  inceptionThreads(50).start
+
+  Thread.sleep(2000 )
 }
