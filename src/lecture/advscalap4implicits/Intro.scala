8d91a2ad21f382546f479693f0d16d27ea1d8d6c
Manoj <manoj.delhi24@gmail.com>
2019-05-02 06:55:09 +0700

    Adding example for implicit parameters.

--- src/lecture/advscalap4implicits/Intro.scala
+++ src/lecture/advscalap4implicits/Intro.scala
@@ -8,11 +8,16 @@ object Intro extends App {
 
   implicit def stringToPerson(name: String): Person = Person(name)
 
-  println("Tom".greet) // compiler do the conversion with available implicit
+//  println("Tom".greet) // compiler do the conversion with available implicit
 
   // Having multiple possible implicit, compiler will error, ex:
   class Tmp {
     def greet = 24
   }
 //  implicit def stringToTmp(name: String): Tmp= new Tmp
+
+  /* Implicit parameters : NOT same as default arguments */
+  def increment(value: Int)(implicit amount: Int) = amount + value
+  implicit val defaultAmount = 42
+  println(increment(2))
 }
