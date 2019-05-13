3fc55d9098ccc4e424fff3a5f7f5c7656ac2bdc9
Manoj <manoj.delhi24@gmail.com>
2019-05-13 08:50:32 +0700

    Adding notes for implicits lookup and priority by compiler.

--- src/lecture/advscalap4implicits/OrganisingImplicits.scala
+++ src/lecture/advscalap4implicits/OrganisingImplicits.scala
@@ -2,14 +2,15 @@ package lecture.advscalap4implicits
 
 object OrganisingImplicits extends App {
   // We'll focus on how compiler looks for implicits and where, and its priorities
-  // ex: sort method
+  // ex: sort method - when we don't define anything it picks up form scala.Predef
+  //      and when we define more than 1 implicit ordering for Int it gives error.
   implicit val decrOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _ )
 //  print(List(2,4,1,5,3).sorted) // takes implicit ordering form scala.Predef
 
-  /* Potential implicits:
+  /* Potential implicits (used as implicit parameters):
       - val/var
       - object
-      - accessor methods = defs with no parentheses[Its very important]
+      - accessor methods: defs with no parentheses[Its very important]
         - if we change the above code to below, it'll work, but
             implicit def decrOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _ )
         - if we change it to this, then it'll not work. So no parentheses is important
@@ -23,14 +24,21 @@ object OrganisingImplicits extends App {
     Person("Dik", 32),
     Person("Harry", 42)
   )
-  implicit val peopleByName: Ordering[Person] = Ordering.fromLessThan((x, y) => x.name.compareTo(y.name) < 0)
-  println(people.sorted)
+  object Person {
+    implicit val peopleByName: Ordering[Person] = Ordering
+      .fromLessThan((x, y) => x.name.compareTo(y.name) < 0)
+  }
+
+  println(people.sorted) // if we dont define any implicit ordering it'll fail: No implicit Ordering defined error.
 
   /* Implicit scope: places where compiler searches for implicit values
       - Local scope
       - imported scope
-      - companions of all types involved = A, T or any supertype, ex:
+      - companions of all types involved in method signature
+        A, T or any supertype, ex:
           def sorted[B >: A](implicit ord: Ordering[B]): List
+        In our case, compiler looks for List, Ordering, Person companion objects.
+
         If we define the above implicit inside object Abc { } it'll not be available but
         if we define it inside object Person it'll be a available since Person type in involved in Ordering.
   */
