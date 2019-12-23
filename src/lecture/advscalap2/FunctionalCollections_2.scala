2c7d756063015738f7c5192345811137fa1c0c76
Manoj <sehrawat.manoj24@gmail.com>
2019-12-23 07:53:02 +0700

    Adding subtract, intersection and difference to my functional set

--- src/lecture/advscalap2/FunctionalCollections_2.scala
+++ src/lecture/advscalap2/FunctionalCollections_2.scala
@@ -14,8 +14,8 @@ object FunctionalCollections_2 extends App {
     def apply(max: Int = 26): String = chars(rand.nextInt(max % 26))
   }
 
-  val randChar = new MyRandomChar()
-  println(1 to 12 map { _ => randChar(4) })
+  // val randChar = new MyRandomChar()
+  // println(1 to 12 map { _ => randChar(4) })
 
   trait MySet[A] extends (A => Boolean) {
     override def apply(v1: A): Boolean = contains(v1)
@@ -27,6 +27,16 @@ object FunctionalCollections_2 extends App {
     def flatMap[B](fn: A => MySet[B]): MySet[B]
     def filter(predicate: A => Boolean): MySet[A]
     def foreach(fn: A => Unit): Unit
+
+    /* Exercise
+        - removing element from set
+        - intersection with another set : return common elements
+        - difference with another set : remove common elements
+    * */
+    def -(i: A): MySet[A]
+    def &(anotherSet: MySet[A]): MySet[A]
+    def --(anotherSet: MySet[A]): MySet[A]
+
   }
   // Small exercise to implement this MySet trait.
   class EmptyMySet[A] extends MySet[A] {
@@ -38,6 +48,11 @@ object FunctionalCollections_2 extends App {
     def flatMap[B](fn: A => MySet[B]): MySet[B] = new EmptyMySet[B]
     def filter(predicate: A => Boolean): MySet[A] = this
     def foreach(fn: A => Unit): Unit = ()
+
+    def -(i: A): MySet[A] = this // throw new RuntimeException("Cannot remove item from empty set")
+    def &(anotherSet: MySet[A]): MySet[A] = this
+    def --(anotherSet: MySet[A]): MySet[A] = this
+
   }
 
   class MySetImpl[A](head: A, tail: MySet[A] = new EmptyMySet[A]) extends MySet[A] {
@@ -56,7 +71,20 @@ object FunctionalCollections_2 extends App {
       fn(head)
       tail foreach fn
     }
+
+    def -(i: A): MySet[A] =
+      if (head == i) tail
+      else tail - i + head
+
+    def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+    def --(anotherSet: MySet[A]): MySet[A] = filter(x => !anotherSet(x))
   }
-  val set1 = new EmptyMySet[Int] + 1 + 2 + 1 ++ new MySetImpl[Int](3) ++ new MySetImpl[Int](2)
-  set1 flatMap(x => new MySetImpl[Int](x) + x * 10 ) filter(_ % 2 == 0) foreach println
+  // val set1 = new EmptyMySet[Int] + 1 + 2 + 1 ++ new MySetImpl[Int](3) ++ new MySetImpl[Int](2)
+  // set1 flatMap(x => new MySetImpl[Int](x) + x * 10 ) filter(_ % 2 == 0) foreach println
+  val set1 = new MySetImpl[Int](3) + 5 + 1
+  // set1 - 5 foreach println
+  val set2 = new MySetImpl[Int](2) + 3 + 4 + 5
+  // set1 & set2 foreach println
+  set1 -- set2 foreach println
 }
