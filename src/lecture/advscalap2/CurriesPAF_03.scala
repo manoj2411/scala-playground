b6c666de57e920ad92385c5d436d6b7f52cf713b
Manoj <sehrawat.manoj24@gmail.com>
2020-01-29 08:45:27 +0100

    Adding seq number to file. Enhancing note, example for lifting and method vs Function value.

--- src/lecture/advscalap2/CurriesPAF_03.scala
+++ src/lecture/advscalap2/CurriesPAF_03.scala
@@ -1,7 +1,7 @@
 package lecture.advscalap2
 
 // PAF - Partially Applied Function
-object CurriesPAF extends App {
+object CurriesPAF_03 extends App {
 
   // curried functions: functions returning other function as results.
   val superAdder: Int => Int => Int =
@@ -12,13 +12,19 @@ object CurriesPAF extends App {
 
   def curriedAdder(x: Int)(y: Int) = x + y
   /* val adder7 = curriedAdder(7) // throws error
-  - we are attempting to call this method with fewer params list then the compiler expects.
-  - Compiler is confused here, are we creating a function here or trying to get the value of adder.
-  - If make the type annotation then it'll be ok like:
+  - curriedAdder is a method bcz its defined with `def`, not a Function.
+    - method is always called with all parameters
+    - we are attempting to call this method with fewer params list then the compiler expects.
+    - Compiler is confused here, are we creating a Function here or trying to get the value of adder.
+  - We want to use Function values in HOF, this is whole purpose of Functional Programming.
+     - we can use methods to HOF unless they are transformed to Function value (Lifted)
+     - JVM limitation - methods are part of instances of class (or object i.e. singleton object)
+     - methods are not instances of functions.
+  - If make the type annotation then it'll be ok like (it does the lifting):
   */
   val adder7: Int => Int = curriedAdder(7)
 
-  /* Lifting: transforming a method to function/function value is called lifting, also called ETA-EXPANSION
+  /* Lifting: transforming a method to Function/Function value is called lifting, also called ETA-EXPANSION
       - functions != methods (JVM limitation)
   */
   def inc(s: Int) = s + 1
@@ -43,7 +49,7 @@ object CurriesPAF extends App {
   val add7_5 = simpleAddMethod(7, _: Int) // alternative syntax for turning method into function values
   val add7_6 = simpleAddFn(7, _: Int)
 
-  // Underscore is very powerful
+  /*      Underscore is very powerful     */
   def concat(x: String, y: String, z: String) = x + y + z
   val insertName = concat("Hello, I am ", _: String, ". How are you?") // x: String => concat(hey, x, howAreYou)
   println(insertName("Manoj"))
@@ -53,7 +59,7 @@ object CurriesPAF extends App {
 
   /* Exercise:
   1. Process list of number and return string representation with different formats
-      User %4.2f, %8.6f and %14.12f with a curried formatter Fn. ex: println("%8.6f".format(22/7))
+      Use %4.2f, %8.6f and %14.12f with a curried formatter Fn. ex: println("%8.6f".format(22/7))
         println("PI: [%4.2f]".format(22/7f))
         println("PI: [%8.6f]".format(22/7f))
         println("PI: [%14.12f]".format(22/7f))
@@ -69,6 +75,36 @@ object CurriesPAF extends App {
   println(list.map(smallFormat))
   println(list.map(midFormat))
   println(list.map(bigFormat))
+
+  /*    Difference between parameters: by-name vs 0-lambda     */
+  def byName(num: => Int) = num + 1
+  def byFunction(fn: () => Int) = fn() + 1
+
+  def method: Int = 42
+  def methodWithParen(): Int  = 42
+  /*
+      Calling byName and byFunction
+        - int
+        - method
+        - methodWithParen
+        - lambda
+        - PAF
+  */
+  byName(24)
+  byName(method) // method is evaluated and result is returned
+  byName(methodWithParen()) // method is evaluated and result is returned
+  val lam1 = () => 24
+  // byName(lam1) // it expects a value but its a Function
+  // byName(methodWithParen _)
+
+  // byFunction(24) // expects a Function value as param
+  // byFunction(method) // method is not Function value
+  // byFunction(methodWithParen()) // method is not Function value - need to lift
+  byFunction(methodWithParen _) // works with lifting
+  byFunction(methodWithParen)  // this is same as above
+  byFunction(lam1)
+  byFunction(methodWithParen _)  // this is same as above
+
 
 
 
