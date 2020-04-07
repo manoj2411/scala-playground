c4cbafd07d1c154a4a8411a8de31c24b2ddd9f58
Manoj <sehrawat.manoj24@gmail.com>
2020-04-07 13:23:20 +0100

    adding seq number to implicits Intro

--- src/lecture/advscalap4implicits/Intro_01.scala
+++ src/lecture/advscalap4implicits/Intro_01.scala
@@ -1,14 +1,15 @@
 package lecture.advscalap4implicits
 
-object Intro extends App {
+object Intro_01 extends App {
 
   case class Person(name: String) {
     def greet = s"Greetings, I am $name"
   }
 
   implicit def stringToPerson(name: String): Person = Person(name)
+  /* To remove the need of above method scala 2.10 introduced implicit class */
 
-//  println("Tom".greet) // compiler do the conversion with available implicit
+  //  println("Tom".greet) // compiler do the conversion with available implicit
 
   // Having multiple possible implicit, compiler will error, ex:
   class Tmp {
